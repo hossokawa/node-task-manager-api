6f54c5d9859b6472bce5d59f9aea0db0771df493
Hossokawa <leonardoholiver@gmail.com>
2024-02-21 11:00:00 -0300

    edit connection func

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connStr =
-  "mongodb+srv://hossokawa:B47f5pq91@cluster0.jvzpmfu.mongodb.net/NodeTaskManager?retryWrites=true&w=majority";
-
 const connectDB = (uri) => {
   return mongoose.connect(uri);
 };
